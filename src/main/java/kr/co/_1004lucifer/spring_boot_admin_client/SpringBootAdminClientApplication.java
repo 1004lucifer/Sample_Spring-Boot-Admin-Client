43a5d190364d5512b463791ca4af700247dd42c2
Wook Huh <1004lucifer@gmail.com>
2020-03-23 19:15:57 +0900

    first commit

---
+++ src/main/java/kr/co/_1004lucifer/spring_boot_admin_client/SpringBootAdminClientApplication.java
@@ -0,0 +1,13 @@
+package kr.co._1004lucifer.spring_boot_admin_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientApplication.class, args);
+    }
+
+}
